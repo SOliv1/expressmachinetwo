ea4eee5ff9e5728694cdca7a5e6d4fc99658b9c2
SOliv1 <s.oliver1@icloud.com>
2022-01-18 15:37:52 +0000

    lets work

---
+++ public/test/test.js
@@ -0,0 +1,21 @@
+console.log = () => {};
+import rewire from 'rewire';
+import { expect } from 'chai';
+import request from 'supertest';
+
+describe('', function() {
+  it('', function(done) {
+    process.env.PORT = 8000;
+    const appModule = rewire('../app.js');
+    const app = appModule.__get__('app');
+    const expressions = appModule.__get__('animals');
+    request(app)
+    .get('/animals')
+    .then((response) => {
+      expect(response.status, 'Did you send a response from the GET /animals route?').to.equal(200);
+      expect(response.body, 'Did you send back the animals array?').to.deep.equal(expressions);
+      done();
+    })
+    .catch(done);
+  });
+});
